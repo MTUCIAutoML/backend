a208e07f76e4d7d505bb5833e2cfe32a36b81270
Alesya Moseva <mosevaa13@gmail.com>
2024-06-07 13:25:43 +0300

    add rrouters

--- src/routers/__init__.py
+++ src/routers/__init__.py
@@ -4,6 +4,7 @@ from routers.pipeline import router as pipeline
 from routers.train import router as train
 from routers.login import router as login
 from routers.dataset import router as dataset
+from routers.device import router as device
 
 router = APIRouter()
 
@@ -11,3 +12,4 @@ router.include_router(pipeline, prefix='/pipeline', tags=['Pipeline'])
 router.include_router(train, prefix='/train', tags=['Train'])
 router.include_router(login, prefix='/user', tags=['Login'])
 router.include_router(dataset, prefix='/dataset', tags=['Dataset'])
+router.include_router(device, prefix='/device', tags=['Device'])
